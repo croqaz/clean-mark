2cd5b42f6b161553aae924a2f9d6bd9585b3c5e9
Cristi Constantin <cristi.constantin@sent.com>
2021-03-19 16:32:48 +0000

    Updated the tests

--- test/clean.tests.js
+++ test/clean.tests.js
@@ -11,13 +11,14 @@ describe('Readability Test suite', function () {
       if (dir[0] === '.') continue
       const wanted = JSON.parse(fs.readFileSync(`test/fixtures/${dir}/expected.json`, 'utf8'))
       const p = clean(`test/fixtures/${dir}/source.htm`)
-      .then(res => {
-        res.author.should.equal(wanted.author)
-        res.title.should.equal(wanted.title)
-        for (const m of wanted.matches) {
-          res.text.should.containEql(m)
-        }
-      })
+        .then(res => {
+          res.author.should.equal(wanted.author)
+          res.title.should.equal(wanted.title)
+          // fs.writeFileSync(`test-${dir}.md`, res.text.trim() + '\n\n')
+          for (const m of wanted.matches) {
+            res.text.should.containEql(m)
+          }
+        })
       process.stdout.write('.')
       promises.push(p)
     }
@@ -41,7 +42,7 @@ describe('Readability Test suite', function () {
   })
 
   it('Article from TechRadar.com', function (done) {
-    clean('http://www.techradar.com/news/amd-vs-intel-the-16-core-showdown', {fileType: 'text'})
+    clean('http://www.techradar.com/news/amd-vs-intel-the-16-core-showdown', { fileType: 'text' })
       .then(res => {
         res.text.should.containEql('The past few years have seen something of a plateau')
         res.text.should.containEql('AMD Ryzen Threadripper 1950X')
@@ -52,11 +53,11 @@ describe('Readability Test suite', function () {
 
   it('Article from Yahoo News', function (done) {
     clean('https://www.yahoo.com/news/tomb-drawing-shows-mongoose-leash-142300393.html')
-    .then(res => {
-      res.text.should.containEql('A mongoose on a leash, a colorful pelican and various bats are just a few')
-      res.text.should.containEql('researchers are recording and analyzing additional creatures')
-      res.text.should.containEql('not as hot and dry.')
-      done()
-    }).catch(e => done(e))
+      .then(res => {
+        res.text.should.containEql('A mongoose on a leash, a colorful pelican and various bats are just a few')
+        res.text.should.containEql('researchers are recording and analyzing additional creatures')
+        res.text.should.containEql('not as hot and dry.')
+        done()
+      }).catch(e => done(e))
   })
 })
